8cacfb1df1d16ab3caf879b33895369722ef085f
scholark <ksrt@outlook.com>
2019-09-26 02:48:51 +0530

    modified _gitsetup.sh

--- _gitsetup.sh
+++ _gitsetup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # SETTING UP GIT FOR FIRST TIME IN A SYSTEM
 
-git config --global user.name "scholark"
-git config --global user.email "ksrt@outlook.com"
+#git config --global user.name "scholark"
+#git config --global user.email "xxxx@outlook.com"
 #git config --global color.ui auto
 
 git init
@@ -11,6 +11,7 @@ git status
 git add -A
 git commit -m "First commit to this repository"
 
-#git push
+#git remote add origin https://github.com/scholarKSR/Anonymous.git
+#git push -u origin master
 
 
